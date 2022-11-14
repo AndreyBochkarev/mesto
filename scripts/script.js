d1392fc1eb2aaa3d590ac6e23ec8b1811a39faf0
Andrey Bochkarev <abochkarev@inbox.ru>
2022-11-14 22:00:15 +0400

    first review

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector('.popup'),
+    popupEdit = document.querySelector('.profile__edit'),
+    popupName = document.querySelector('.popup__input_name'),
+    popupProfession = document.querySelector('.popup__input_profession'),
+    popupSubmit = document.querySelector('.popup__submit'),
+    popupClose = document.querySelector('.popup__closed');
+
+const popupCredit = document.querySelector('.profile__name'),
+    popupJob = document.querySelector('.profile__profession');
+
+function openPopup() {
+    popup.classList.add('popup__opened');
+    popupName.value = popupCredit.textContent;
+    popupProfession.value = popupJob.textContent;
+};
+
+function closePopup() {
+    popup.classList.remove('popup__opened');
+};
+
+let popupForm = document.querySelector('.popup__form');
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    popupCredit.textContent = popupName.value;
+    popupJob.textContent = popupProfession.value;
+    closePopup();
+};
+
+popupForm.addEventListener('submit', formSubmitHandler);
+popupClose.addEventListener('click', closePopup);
+popupEdit.addEventListener('click', openPopup);
